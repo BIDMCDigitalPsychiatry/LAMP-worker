d3086d68e0fbd01088ec367e0e9178371e803a43
ITHQ1861\linoy.gopal <linoy.gopal@zcocorporation.com>
2023-01-11 22:54:19 +0530

    #fixes add title in notification to handle language transalation

--- src/queue/SchedulerQueue.ts
+++ src/queue/SchedulerQueue.ts
@@ -149,6 +149,7 @@ export function sendNotification(device_token: string, device_type: string, payl
                 expiration: 10,
               },
               notificationId: `${notificationId}`,
+              title: `${payload.title}.`,
               expiry: 21600000,
               page: `${url}`,
               actions: [{ name: "Open App", page: `${url}` }],
